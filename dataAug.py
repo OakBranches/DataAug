e17a18214fabfbaae8a6433810398e47bd694248
OakBranches <matheusramosimperador@gmail.com>
2022-10-05 13:09:36 -0300

    Criando a classe Augmentation

--- dataAug.py
+++ dataAug.py
@@ -3,58 +3,83 @@ import numpy as np
 import os
 import imutils
 
+# A classe Augmentation é responsável pelas funções 
+# estáticas responsáveis pelo data aug
+class Augmentation:
+    # Aplica um filtro em uma imagem
+    def applyFilter(image: np.ndarray, filter: str) -> np.ndarray:
+        # Aplica o filtro de ruido
+        if filter == 'noise':
+            # cria uma matriz de zeros do mesmo tamanho de 
+            # uma camada da imagem
+            noise = np.zeros((image.shape[0], image.shape[1]), 
+                dtype=np.uint8)
+            
+            # Preenche a matriz com valores aleatórios
+            cv2.randn(noise, 0, 40)
 
-def dataAug(imagePath: str, filtersStr: str):
+            # Converte o tipo da matriz para matriz de inteiros uint8
+            noise = noise.astype(np.uint8)
 
-    # Inicializa os paths a serem utilizados
-    outputPath = imagePath.replace('images', 'output', 1)
-    outputPath = outputPath.replace('.jpg', f'_{filtersStr}', 1)
-
-    # Verifica se a pasta output existe
-    # Se não existir, cria a pasta
-    if not os.path.exists('output'):
-        os.makedirs('output')
-
-    # Cria a lista de filtros a serem aplicados
-    filters: list[str] = filtersStr.split('|')
-
-    # Carrega a imagem
-    image: cv2.Mat = cv2.imread(imagePath)
-
-    # Aplica o filtro de ruido
-    if 'random_noise' in filters:
-        # cria uma matriz de zeros do mesmo tamanho de 
-        # uma camada da imagem
-        noise = np.zeros((image.shape[0], image.shape[1]), 
-            dtype=np.uint8)
+            # Percorre os canais da imagem adicionando ruido
+            for i in range(image.shape[2]):
+                image[:, :, i] = cv2.add(image[:, :, i], noise)
         
-        # Preenche a matriz com valores aleatórios
-        cv2.randn(noise, 0, 40)
+        # Aplica o filtro flip que inverte a imagem horizontalmente
+        elif filter == 'flip':
+            image = cv2.flip(image, 1)
 
-        # Converte o tipo da matriz para matriz de inteiros uint8
-        noise = noise.astype(np.uint8)
+        # Aplica o filtro que rotaciona a imagem
+        elif filter == 'random_rotation':
+            # Gera um angulo aleatório entre 0 e 360
+            angle = np.random.randint(0, 360)
+            # Rotaciona a imagem
+            image = imutils.rotate_bound(image, angle)
+        
+        # Aplica o filtro que transforma a imagem em escala de cinza
+        elif filter == 'grayscale':
+            # Converte a imagem para um canal de cor
+            image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+            # Converte de volta para 3 canais de cor
+            image = cv2.cvtColor(image, cv2.COLOR_GRAY2BGR)
+        
+        return image
 
-        # Percorre os canais da imagem adicionando ruido
-        for i in range(image.shape[2]):
-            image[:, :, i] = cv2.add(image[:, :, i], noise)
-    
-    # Aplica o filtro flip que inverte a imagem horizontalmente
-    if 'flip' in filters:
-        image = cv2.flip(image, 1)
+    # Aplica todos os filtros em uma imagem
+    def applyAllFilters(image: np.ndarray, filtersStr: str) -> np.ndarray:
+        # Cria a lista de filtros a serem aplicados
+        filters: list[str] = filtersStr.split('|')
 
-    # Aplica o filtro que rotaciona a imagem
-    if 'random_rotation' in filters:
-        # Gera um angulo aleatório entre 0 e 360
-        angle = np.random.randint(0, 360)
-        # Rotaciona a imagem
-        image = imutils.rotate_bound(image, angle)
-    
-    # Aplica o filtro que transforma a imagem em escala de cinza
-    if 'greyscale' in filters:
-        image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        # Aplica os filtros na imagem
+        for filter in filters:
+            image = Augmentation.applyFilter(image, filter)
 
-    # Salva a imagem desejada
-    cv2.imwrite(f'{outputPath}.jpg', image)
+        return image
+  
+    # Carrega, aplica os filtros e salva, retorna o nome do arquivo
+    def dataAugAndSave(imagePath: str, filtersStr: str, imageSrc: np.ndarray = None) -> str:
+        # Carrega a imagem se não tiver sido passada como parâmetro
+        imageSrc: imageSrc or cv2.Mat = cv2.imread(imagePath)
+
+        # Aplica os filtros na imagem
+        image = Augmentation.applyAllFilters(imageSrc, filtersStr)
+
+        # Inicializa os paths a serem utilizados
+        outputPath = imagePath.replace('images', 'output', 1)
+        outputPath = outputPath.replace('.jpg', f'_{filtersStr}', 1)
+
+        # Verifica se a pasta output existe
+        # Se não existir, cria a pasta
+        if not os.path.exists('output'):
+            os.makedirs('output')
+
+        # Cria o nome do arquivo
+        nome = f'{outputPath}.jpg'
+
+        # Salva a imagem desejada
+        cv2.imwrite(nome, image)
+
+        return nome
 
 
-# dataAug('images/video_1_mp4_60.jpg', 'random_noise|greyscale|flip|random_rotation')
+# Augmentation.dataAugAndSave('images/video_1_mp4_60.jpg', 'random_noise|greyscale|flip|random_rotation')
